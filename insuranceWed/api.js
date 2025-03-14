dd42ad8bcb12f2daf28f48825713520a69f6748a
Moza <moza13571025@gmail.com>
2025-03-14 21:17:42 +0800

    優化UI：將原生彈跳視窗(無法移動位置)，改為Bootstrap的modal。

--- insuranceWed/api.js
+++ insuranceWed/api.js
@@ -34,8 +34,11 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((data) => {
         console.log("伺服器回應：", data);
 
-        // 顯示彈跳視窗
-        alert("感謝填寫，我們會盡快跟您聯絡。");
+        // 使用 Bootstrap 5 Modal 取代 alert()
+        let thankYouModal = new bootstrap.Modal(
+          document.getElementById("thankYouModal")
+        );
+        thankYouModal.show();
 
         // 清空表單
         form.reset();
@@ -46,39 +49,3 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
 });
-
-// let sendButton = document.querySelector("button");
-
-// function send(event) {
-//   event.preventDefault(); // 防止表單默認提交行為
-
-//   let name = document.querySelector("#nameValue").value;
-//   let email = document.querySelector("#emailValue").value;
-//   let demand = document.querySelector("#demandValue").value;
-
-//   fetch(
-//     "https://script.google.com/macros/s/AKfycbzNg4n7KsnEWv__5GgEhfUJQtHH3bdY9SIDFBYDu_8OAK_YpaHSgSvyXlQvgKcZBBLT/exec",
-//     {
-//       method: "POST",
-//       mode: "no-cors", // **關閉 CORS**
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         name: name,
-//         email: email,
-//         demand: demand,
-//       }),
-//     }
-//   )
-//     .then((response) => response.text()) // 轉換成純文字
-//     .then((data) => {
-//       console.log("伺服器回應：", data); // 可以在 Console 看 API 回應
-//       if (data === "成功") {
-//         alert("成功");
-//       }
-//     })
-//     .catch((error) => console.error("錯誤：", error));
-// }
-
-// sendButton.addEventListener("click", send);
